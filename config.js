d765aa179dc5c79038f67ece69eb175d2090232f
ekeric13 <ekeric13@gmail.com>
2015-12-05 17:17:15 -0800

    Finish styling facebook

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 keys = {};
-keys.embedlyKey = "";
+keys.embedlyKey = "ef2e038a54184a57a182c1ed5b38fa33";
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function ask_page_data() {
-  var key = 'ef2e038a54184a57a182c1ed5b38fa33';
   var tabid = location.hash.slice(1);
   tabid = parseInt(tabid);
   var content = JSON.parse(window.localStorage['parsedData']);
+  console.log(content);
 
   $('title').html(content.title);
   // var html = '<!DOCTYPE html><html><head><title>' + content.title + '</title>' + '<meta charset="utf-8">' + '</head><body>';
@@ -10,9 +10,50 @@ function ask_page_data() {
   html += content.content; 
   // html += '</body></html>';
   $('#parsed').html(html);
+  var allImages = $('img');
+  var contentWidth = 700;
+  allImages.on('load', function() {
+    for (var i = 0; i < allImages.length; i++) {
+      var image = allImages[i];
+      var imageWidth = image.clientWidth;
+      var imageWidthFinal = ( (imageWidth-contentWidth) /2) + "px";
+      // $(image).css({'right': imageWidthFinal});
+      image.style.right = imageWidthFinal;
+    }
+
+  });
+  
 }
 
+$('.medium-styles').on('click', function() {
+  if ( $('link')[1].getAttribute('href') === "styles.css" ) {
+
+  } else {
+    $('link')[1].remove();
+    var fileref = document.createElement("link");
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", 'styles.css');
+    document.getElementsByTagName("head")[0].appendChild(fileref);
+  }
+});
+
+$('.fb-styles').on('click', function() {
+
+  if ( $('link')[1].getAttribute('href') === "styles2.css" ) {
+
+  } else {
+    $('link')[1].remove();
+    var fileref = document.createElement("link");
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", 'styles2.css');
+    document.getElementsByTagName("head")[0].appendChild(fileref);
+  }
+});
+
 ask_page_data();
+
 
 // function data_received(resp) {
 //   var url = remove_url_prefix(resp.url);

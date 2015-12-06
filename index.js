afdf113bc1dee917820455009b359ec4250490a7
Derrick J Chie <djchie.dev@gmail.com>
2015-12-05 17:06:53 -0800

    Moves API calls to the load.js; removes reliance on localStorage

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-function ask_page_data() {
-  var key = 'ef2e038a54184a57a182c1ed5b38fa33';
-  var tabid = location.hash.slice(1);
-  tabid = parseInt(tabid);
-  var content = JSON.parse(window.localStorage['parsedData']);
-
-  $('title').html(content.title);
-  // var html = '<!DOCTYPE html><html><head><title>' + content.title + '</title>' + '<meta charset="utf-8">' + '</head><body>';
-  var html = '<h1>' + content.title + '</h1>';
-  html += content.content; 
-  // html += '</body></html>';
-  $('#parsed').html(html);
-}
-
-ask_page_data();
-
-// function data_received(resp) {
-//   var url = remove_url_prefix(resp.url);
-//   $(document.body).append('<div>'+url+"</div>");
-// }
-
-// function remove_url_prefix(url) {
-//   if (url.startsWith("http://"))
-//       url = url.substr(7);
-//   else if (url.startsWith("https://"))
-//       url = url.substr(8);
-//   return url;
-// }
+// Listens for messages
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // When the 'on_new_tab' is received
+  if ( request.message === 'on_new_tab' ) {
+    // Make a request to the Embedly Extract API
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://api.embed.ly/1/extract?key=' + request.key + '&url=' + request.url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() { 
+      // Inject the new content
+      var content = JSON.parse(xhr.responseText);
+      $('title').html(content.title);
+      var html = '<h1>' + content.title + '</h1>';
+      html += content.content; 
+      $('#beautified').html(html);
+    };
+    xhr.send();
+  }
+});

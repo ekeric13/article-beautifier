f3301067bef38959f9e0b87b9aad00ed00e772d8
Derrick J Chie <djchie.dev@gmail.com>
2015-12-05 17:52:35 -0800

    Moved ajax calls from index.js to background.js; added an icon; added a ton of documentation

---
+++ README.md
@@ -0,0 +1,42 @@
+# Article Beautifier
+
+> A Chrome Extension to strip away all the fluff and advertisements on a news article site so that you can focus and enjoy the article the way it was meant to be presented: distraction-free and elegant.
+
+## Team
+
+  - __Development Team Members__: Derrick Chie and Eric Kennedy
+
+## Table of Contents
+
+1. [Usage](#Usage)
+1. [Requirements](#requirements)
+1. [Development](#development)
+    1. [Running Locally on Chrome](#running-locally-on-chrome)
+    1. [Roadmap](#roadmap)
+1. [Team](#team)
+1. [Contributing](#contributing)
+
+## Usage
+
+> After having the Chrome Extension installed, go to any news article site and hit the Article Beautifier icon to open a new tab with your article beautified!
+
+## Requirements
+
+- Google Chrome
+
+## Development
+
+### Running Locally on Chrome
+
+1. Open up Google Chrome
+1. Navigate to the Extensions Settings page
+1. Click on 'Load unpacked extension...'
+1. Naviate to a cloned version of this repository
+
+### Roadmap
+
+View the project roadmap [here](https://github.com/ekeric13/article-beautifier/issues)
+
+## Contributing
+
+See [CONTRIBUTING.md](CONTRIBUTING.md) for contribution guidelines.
--- background.js
+++ background.js
@@ -15,18 +15,45 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
   // When the 'open_new_tab' is received
   if ( request.message === 'open_new_tab' ) {
-    // Creates a url for the new tab and opens it in the new tab
-    var url = chrome.extension.getURL('index.html#'+ request.tabId);
-    chrome.tabs.create({
-      'url': url
-    }, function (tab) {
-      // Sends a message 'clicked_browser_action' to the new tab with url to parse
-      chrome.tabs.sendMessage(tab.id, {
-        'message': 'on_new_tab',
-        'tabId': request.tabId,
-        'url': request.url,
-        'key': keys.embedlyKey
+    // Make a request to the Embedly Extract API
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://api.embed.ly/1/extract?key=' + keys.embedlyKey + '&url=' + request.url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() { 
+      // Creates a url for the new tab and opens it in the new tab
+      var url = chrome.extension.getURL('index.html#'+ request.tabId);
+      chrome.tabs.create({
+        'url': url
+      }, function (tab) {
+        // Sends a message 'clicked_browser_action' to the new tab with url to parse
+        chrome.tabs.sendMessage(tab.id, {
+          'message': 'on_new_tab',
+          'tabId': request.tabId,
+          'content': xhr.responseText
+        });
       });
-    });
+    };
+    xhr.send();
+
   }
 });
+
+// Listens for messages (Having request in load instead of background.js)
+// chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+//   // When the 'open_new_tab' is received
+//   if ( request.message === 'open_new_tab' ) {
+//     // Creates a url for the new tab and opens it in the new tab
+//     var url = chrome.extension.getURL('index.html#'+ request.tabId);
+//     chrome.tabs.create({
+//       'url': url
+//     }, function (tab) {
+//       // Sends a message 'clicked_browser_action' to the new tab with url to parse
+//       chrome.tabs.sendMessage(tab.id, {
+//         'message': 'on_new_tab',
+//         'tabId': request.tabId,
+//         'url': request.url,
+//         'key': keys.embedlyKey
+//       });
+//     });
+//   }
+// });
